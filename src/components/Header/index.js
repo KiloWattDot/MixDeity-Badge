395a220150f0b10cf59d641acbfa44f3cca21ec2
KiloWattDot <74320819+KiloWattDot@users.noreply.github.com>
2022-06-09 21:30:46 -0400

    Added new components, connected a few to the App.js, and added necessary files

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <section id="experience">
+            <div className='expTitle_box'> 
+                {/* <h5 id='recentWork'>My Recent Work</h5> */}
+                <h2 id='expTitle'>Header Area</h2>
+            </div>
+        </section>
+
+
+    )
+}
+
+
+export default Header;
